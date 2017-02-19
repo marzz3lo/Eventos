d0b2c27c1c0481f108bc851740f37f562f0b7e62
marzz3lo <marzzelo@gmail.com>
2017-02-18 20:17:36 -0400

    Ejercicio: Suscribir y dar de baja a usuarios en temas en FCM.

--- app/src/main/java/com/mostudios/eventos/MainActivity.java
+++ app/src/main/java/com/mostudios/eventos/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mostudios.eventos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -39,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
                 databaseReference);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     private boolean comprobarGooglePlayServices() {
@@ -71,17 +78,34 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Bundle extras = getIntent().getExtras();
 
-        if (extras!=null && extras.keySet().size()>4) {
-            String evento="";
-            evento ="Evento: "+extras.getString("Evento")+ "\n";
-            evento = evento + "Día: "+ extras.getString("Dia")+ "\n";
-            evento = evento +"Ciudad: "+extras.getString("Ciudad")+ "\n";
-            evento = evento +"Comentario: "+extras.getString("Comentario");
+        if (extras != null && extras.keySet().size() > 4) {
+            String evento = "";
+            evento = "Evento: " + extras.getString("Evento") + "\n";
+            evento = evento + "Día: " + extras.getString("Dia") + "\n";
+            evento = evento + "Ciudad: " + extras.getString("Ciudad") + "\n";
+            evento = evento + "Comentario: " + extras.getString("Comentario");
             mostrarDialogo(getApplicationContext(), evento);
             for (String key : extras.keySet()) {
                 getIntent().removeExtra(key);
             }
             extras = null;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_temas) {
+            Intent intent = new Intent(getBaseContext(), Temas.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
