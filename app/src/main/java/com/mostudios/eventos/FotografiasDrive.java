a5813b964a7a4d814622b816726e6b4d0c7971db
marzz3lo <marzzelo@gmail.com>
2017-02-22 00:16:14 -0400

    Ejercicio: Obtener acceso a Google Drive.

--- app/src/main/java/com/mostudios/eventos/FotografiasDrive.java
+++ app/src/main/java/com/mostudios/eventos/FotografiasDrive.java
@@ -1,12 +1,28 @@
 package com.mostudios.eventos;
 
+import android.accounts.AccountManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Arrays;
 
 /**
  * Created by marzzelo on 21/2/2017.
@@ -15,6 +31,20 @@ import android.widget.TextView;
 public class FotografiasDrive extends AppCompatActivity {
     public TextView mDisplay;
     String evento;
+    static Drive servicio = null;
+    static GoogleAccountCredential credencial = null;
+    static String nombreCuenta = null;
+    static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    static final String DISPLAY_MESSAGE_ACTION = "com.mostudios.eventos.DISPLAY_MESSAGE";
+    private static Handler manejador = new Handler();
+    private static Handler carga = new Handler();
+    private static ProgressDialog dialogo;
+    private Boolean noAutoriza = false;
+    static final int SOLICITUD_SELECCION_CUENTA = 1;
+    static final int SOLICITUD_AUTORIZACION = 2;
+    static final int SOLICITUD_SELECCIONAR_FOTOGRAFIA = 3;
+    static final int SOLICITUD_HACER_FOTOGRAFIA = 4;
+    private static Uri uriFichero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +54,19 @@ public class FotografiasDrive extends AppCompatActivity {
         evento = extras.getString("evento");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        credencial = GoogleAccountCredential.usingOAuth2(this, Arrays.asList(DriveScopes.DRIVE));
+        SharedPreferences prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+        nombreCuenta = prefs.getString("nombreCuenta", null);
+        noAutoriza = prefs.getBoolean("noAutoriza", false);
+        if (!noAutoriza) {
+            if (nombreCuenta == null) {
+                PedirCredenciales();
+            } else {
+                credencial.setSelectedAccountName(nombreCuenta);
+                servicio = obtenerServicioDrive(credencial);
+            }
+        }
     }
 
     @Override
@@ -43,5 +86,67 @@ public class FotografiasDrive extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    static void mostrarMensaje(final Context context, final String mensaje) {
+        manejador.post(new Runnable() {
+            public void run() {
+                Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    static void mostrarCarga(final Context context, final String mensaje) {
+        carga.post(new Runnable() {
+            public void run() {
+                dialogo = new ProgressDialog(context);
+                dialogo.setMessage(mensaje);
+                dialogo.show();
+            }
+        });
+    }
+
+    static void ocultarCarga(final Context context) {
+        carga.post(new Runnable() {
+            public void run() {
+                dialogo.dismiss();
+            }
+        });
+    }
+
+    private void PedirCredenciales() {
+        if (nombreCuenta == null) {
+            startActivityForResult(credencial.newChooseAccountIntent(),
+                    SOLICITUD_SELECCION_CUENTA);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case SOLICITUD_SELECCION_CUENTA:
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    nombreCuenta = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (nombreCuenta != null) {
+                        credencial.setSelectedAccountName(nombreCuenta);
+                        servicio = obtenerServicioDrive(credencial);
+                        SharedPreferences prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("nombreCuenta", nombreCuenta);
+                        editor.commit();
+                    }
+                }
+                break;
+            case SOLICITUD_HACER_FOTOGRAFIA:
+                break;
+            case SOLICITUD_SELECCIONAR_FOTOGRAFIA:
+                break;
+            case SOLICITUD_AUTORIZACION:
+                break;
+        }
+    }
+
+    private Drive obtenerServicioDrive(GoogleAccountCredential credencial) {
+        return new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credencial).build();
     }
 }
